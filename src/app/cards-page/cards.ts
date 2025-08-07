a9ae3642b9b88a9202ab647252fda01696d3e5cb
Eric <ericbrown589@yahoo.com>
2025-08-07 12:53:46 -0500

    created service, implented functionality for GET cards from backend

--- src/app/cards-page/cards.ts
+++ src/app/cards-page/cards.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { DataService } from "../data-service";
+import { CommonModule } from "@angular/common";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'card-page',
+    imports: [CommonModule],
     templateUrl: './cards.html',
     styleUrl: './cards.css'
 })
 
 export class CardPage implements OnInit {
 
-    constructor(private router: Router, public route: Router) {}
+    cards$!: Observable<any[]>;
+
+
+    constructor(private router: Router, public route: Router, private dataService: DataService) {}
 
     ngOnInit(): void {
-        
+        this.cards$ = this.dataService.getCards();
     }
+
+    
 
     public onClick(value: string) {
         if (value == "deck") {
